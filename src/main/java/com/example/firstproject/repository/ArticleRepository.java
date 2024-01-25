ca6e28fd7f6586587791f229b23b69a43a70ef0a
jalyice <jalyice@gmail.com>
2024-01-25 17:55:30 +0900

    commit

---
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -0,0 +1,15 @@
+package com.example.firstproject.repository;
+
+import com.example.firstproject.entity.Article;
+
+import java.util.ArrayList;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArticleRepository extends CrudRepository<Article, Long>{
+
+    @Override
+    ArrayList<Article> findAll(); 
+    
+    
+}
